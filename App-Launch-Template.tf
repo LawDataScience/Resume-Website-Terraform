d00aa6f374bd6e853921e5eb2715028c739f6317
LawDataScience <135552364+LawDataScience@users.noreply.github.com>
2023-06-28 04:36:39 -0500

    Delete App-Launch-Template.tf

--- App-Launch-Template.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_launch_template" "website_app"{
-    name = "website_app"
-    image_id = "ami-053b0d53c279acc90"
-    key_name = "ec2keypair"
-    vpc_security_group_ids = [aws_security_group.website_app.id]
-}
